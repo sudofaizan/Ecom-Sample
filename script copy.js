bf9bfb9fc4f212997a4f79774f7ea48aa08d7c5b
Faizan quazi <faizan_quazi@intuit.com>
2024-08-18 16:14:41 +0530

    update

---
+++ script copy.js
@@ -0,0 +1,25 @@
+const productsUrl = 'https://raw.githubusercontent.com/sudofaizan/Ecom-Sample/main/products.json';
+const productGrid = document.getElementById('product-grid');
+
+function getProducts() {
+  fetch(productsUrl)
+    .then(response => response.json())
+    .then(products => {
+      products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product');
+        productCard.innerHTML = `
+        <h4>product ID:${product.id}</h4>
+          <img src="${product.image}" alt="${product.name}">
+          <h3>${product.name}</h3>
+          <p>${product.description}</p>
+          <p class="price">$${product.price.toFixed(2)}</p>
+          
+        `;
+        productGrid.appendChild(productCard);
+      });
+    })
+    .catch(error => console.error('Error getting products:', error));
+}
+
+getProducts();
