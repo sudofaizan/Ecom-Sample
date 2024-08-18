6bacb1d4817f1447f7e21a5aa45fcf6d92f3564d
Faizan quazi <faizan_quazi@intuit.com>
2024-08-18 15:22:20 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const productsUrl = 'products.json';
+const productsUrl = 'https://raw.githubusercontent.com/sudofaizan/Ecom-Sample/main/products.json';
 const productGrid = document.getElementById('product-grid');
 
 function getProducts() {
@@ -12,6 +12,7 @@ function getProducts() {
           <img src="${product.image}" alt="${product.name}">
           <h3>${product.name}</h3>
           <p>${product.description}</p>
+          <h4>${product.id}</h4>
           <p class="price">$${product.price.toFixed(2)}</p>
         `;
         productGrid.appendChild(productCard);
