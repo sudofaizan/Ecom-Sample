b1166a4e212d4f2b6e3b695e398bb86b815739e2
Faizan quazi <faizan_quazi@intuit.com>
2024-08-18 15:30:22 +0530

    update

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function getProducts() {
         productCard.innerHTML = `
           <img src="${product.image}" alt="${product.name}">
           <h3>${product.name}</h3>
-          <h3>${product.id}</h3>
           <p>${product.description}</p>
+          <h4>${product.id}</h4>
           <p class="price">$${product.price.toFixed(2)}</p>
         `;
         productGrid.appendChild(productCard);
