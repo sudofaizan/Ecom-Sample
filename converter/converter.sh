0992f1f5c41f5de84acc2e904e9587dedb4f031f
Faizan quazi <faizan_quazi@intuit.com>
2024-08-19 21:22:21 +0530

    upd

--- converter/converter.sh
+++ converter/converter.sh
@@ -7,7 +7,10 @@ function json_to_excel {
 
 function excel_to_json {
     # Use Python to convert XLSX to JSON
-    python3 -c "import pandas as pd; df = pd.read_excel('$1', engine='openpyxl'); df.to_json('$(basename $1 .xlsx).json', orient='records', indent=2)"
+    curl -L "https://docs.google.com/spreadsheets/d/1S2pdJjb8nH7jEf2d93VWMSLS8UkUBB70Mu60lsC5Ulk/export?format=xlsx" -o product.xlsx
+
+    python3 -c "import pandas as pd; df = pd.read_excel('product.xlsx', engine='openpyxl'); df.to_json('../products.json', orient='records', indent=2)"
+    rm -rf product.xlsx
 }
 
 if [[ "$1" == "sheet" ]]; then
