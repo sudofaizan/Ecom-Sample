e5bc02cd6f86fa8799aea44b474d43cd8a58c604
Faizan quazi <faizan_quazi@intuit.com>
2024-08-20 01:25:56 +0530

    upd

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,8 @@
 git pull
-
-docker build -t dexterquazi/ecom --no-cache -f ops/Dockerfile .
+if docker build -t dexterquazi/ecom --no-cache -f ops/Dockerfile .
+then
 docker rm -f $(docker ps -aq)
 docker run -itd --name ecom -p 80:80 dexterquazi/ecom 
+else
+echo "ERROR: job fail in building"
+fi
