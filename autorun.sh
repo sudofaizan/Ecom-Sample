9161c25624ddcda4fc77b86b1fd8802f27920cae
Faizan quazi <faizan_quazi@intuit.com>
2024-08-20 01:24:48 +0530

    upd

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
 git pull
-docker build -t dexterquazi/ecom  --no-cache .
+
 docker build -t dexterquazi/ecom --no-cache -f ops/Dockerfile .
 docker rm -f $(docker ps -aq)
 docker run -itd --name ecom -p 80:80 dexterquazi/ecom 
